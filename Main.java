3072b4559ae4e4cb9d82bb7c37ec39b3a3b3db01
ajaniSterling <ajani.sterling@hgs.hiddengeniusproject.org>
2021-12-11 03:00:04 +0000

    Developed 3.5 Comparing String Object

---
+++ Main.java
@@ -0,0 +1,27 @@
+String name1 = "Alan Turing";
+String name2 = "Ada Lovelace";
+
+if (name1.equals(name2))
+{
+   System.out.println("The names are the same.");
+}
+else
+{
+   System.out.println("The names are not same.");
+}
+
+
+int flag = name1.compareTo(name2);
+
+if (flag == 0)
+{
+   System.out.println("The names are the same.");
+}
+else if (flag < 0)
+{
+   System.out.println("name1 comes before name2.");
+}
+else
+{
+   System.out.println("name2 comes before name1.");
+}
